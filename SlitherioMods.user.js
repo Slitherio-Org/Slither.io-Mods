4801b3d3caa69e0b6a35fc973c9e3e11bb5df012
Slither.io <slitherio.org@gmail.com>
2017-10-26 00:37:58 +0300

    Update SlitherioMods.user.js

--- SlitherioMods.user.js
+++ SlitherioMods.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Slither.io Mods
 // @namespace    http://slitherio.org/
-// @version      3.3
+// @version      3.4
 // @description  Slither.io Mods by Slitherio.Org
 // @author       Slitherio.org
 // @noframes
